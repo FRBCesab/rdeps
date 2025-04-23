af110446537760b73232314a215840a5a9d2abe7
Nicolas Casajus <rdev.nc@gmail.com>
2025-04-23 08:11:30 +0200

    style: use air to style code

--- R/utils-msg.R
+++ R/utils-msg.R
@@ -1,50 +1,43 @@
 #' @noRd
 
 msg_done <- function(..., indent = "") {
-  
   x <- paste(...)
   x <- msg_bullet(x, indent = indent, cli::col_green(cli::symbol$"tick"))
   cli::cat_line(x)
-  
+
   invisible(NULL)
 }
-
 
 
 #' @noRd
 
 msg_oops <- function(..., indent = "") {
-  
   x <- paste(...)
   x <- msg_bullet(x, indent = indent, cli::col_red(cli::symbol$"cross"))
   cli::cat_line(x)
-  
+
   invisible(NULL)
 }
-
 
 
 #' @noRd
 
 msg_info <- function(..., indent = "") {
-  
   x <- paste(...)
   x <- msg_bullet(x, indent = indent, cli::col_blue("(*)"))
   cli::cat_line(x)
-  
+
   invisible(NULL)
 }
-
 
 
 #' @noRd
 
 msg_line <- function(..., indent = "") {
-  
   x <- paste(...)
   x <- paste0(indent, x)
   cli::cat_line(x)
-  
+
   invisible(NULL)
 }
 
@@ -52,47 +45,40 @@ msg_line <- function(..., indent = "") {
 #' @noRd
 
 msg_value <- function(...) {
-  
   x <- unlist(list(...))
-  
+
   if (is.character(x)) {
     x <- encodeString(x, quote = "'")
   }
-  
+
   invisible(paste0(cli::col_blue(x), collapse = ", "))
 }
-
 
 
 #' @noRd
 
 msg_code <- function(...) {
-  
   x <- unlist(list(...))
-  
+
   if (is.character(x)) {
     x <- encodeString(x, quote = "`")
   }
-  
+
   invisible(paste0(cli::col_silver(x), collapse = ", "))
 }
-
 
 
 #' @noRd
 
 msg_bullet <- function(x, indent = "", bullet = cli::symbol$"bullet") {
-  
   bullet <- paste0(indent, bullet, " ")
   msg_indent(x, bullet, "  ")
 }
 
 
-
 #' @noRd
 
-msg_indent <- function (x, first = "  ", indent = first) {
-  
+msg_indent <- function(x, first = "  ", indent = first) {
   x <- gsub("\n\\s", "\n", x)
   x <- gsub("\n", paste0("\n", indent), x)
   paste0(first, x)
--- R/utils-proj.R
+++ R/utils-proj.R
@@ -1,66 +1,65 @@
 #' **Get project root path**
-#' 
+#'
 #' @noRd
 
 path_proj <- function() {
-  
   is_descr_file <- file.exists(file.path(getwd(), "DESCRIPTION"))
-  is_here_file  <- file.exists(file.path(getwd(), ".here"))
+  is_here_file <- file.exists(file.path(getwd(), ".here"))
   is_git_folder <- dir.exists(file.path(getwd(), ".git"))
-  
-  is_rproj_file <- ifelse(length(list.files(getwd(), pattern = "\\.Rproj")) > 0,
-                          TRUE, FALSE)
-  
+
+  is_rproj_file <- ifelse(
+    length(list.files(getwd(), pattern = "\\.Rproj")) > 0,
+    TRUE,
+    FALSE
+  )
+
   if ((is_descr_file + is_here_file + is_git_folder + is_rproj_file) == 0) {
-    stop("It appears that the current working directory is not a valid ", 
-         "project or package. Read more at ", 
-         "'https://usethis.r-lib.org/reference/proj_utils.html'", call. = FALSE)
+    stop(
+      "It appears that the current working directory is not a valid ",
+      "project or package. Read more at ",
+      "'https://usethis.r-lib.org/reference/proj_utils.html'",
+      call. = FALSE
+    )
   }
-  
+
   getwd()
 }
 
 
-
 #' **Check if a DESCRIPTION file exists**
-#' 
+#'
 #' @noRd
 
 check_for_descr_file <- function() {
-  
   path <- path_proj()
-  
+
   if (!file.exists(file.path(path, "DESCRIPTION"))) {
     stop("No 'DESCRIPTION' file found", call. = FALSE)
   }
-  
+
   invisible(NULL)
 }
 
 
-
 #' **Get project name**
-#' 
+#'
 #' @noRd
 
 get_package_name <- function() {
-  
   path <- path_proj()
-  
+
   basename(path)
 }
 
 
-
 #' **Get `roxygen2` package version**
-#' 
+#'
 #' @noRd
 
 get_roxygen2_version <- function() {
-  
   if (!length(find.package("roxygen2", quiet = TRUE))) {
     stop("The package 'roxygen2' is not install", call. = FALSE)
   }
-  
+
   as.character(utils::packageVersion("roxygen2"))
 }
